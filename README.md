fd66ddb9908c3dd2390811ede3fc387c8dad7a34
Andrzej Gebala <andrzej.gebala35@gmail.com>
2021-10-08 16:02:28 +0200

    Stworzenie możliwości dodania nowego zadania

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # projekt-nr7
 
 Zadaniem głównym jest stworzenie TODO listy  
+-Aktywowanie przycisku dodawania oraz wprowadzanie nowych zadań do listy.
+Ostylowanie dodanych elementów zadań w javasrypt, oraz aktywacja enter
--- js.js
+++ js.js
@@ -1,8 +1,13 @@
-let todoInput, errorInfo, addBtn, ulList; 
+let todoInput, errorInfo, addBtn, ulList, liList; 
 
 const main = ()=>{
     DOMElement();
     DOMEvents();
+}
+
+const DOMEvents = ()=>{
+    addBtn.addEventListener('click', addTaskBtn)
+    todoInput.addEventListener('keyup', enterEvent)
 }
 
 const DOMElement = ()=>{
@@ -12,7 +17,43 @@ const DOMElement = ()=>{
     ulList = document.querySelector('.todolist ul');
 }
 
-const DOMEvents = ()=>{
+const addTaskBtn = ()=>{
+    if(todoInput.value !== ''){
+        liList= document.createElement('li');
+        liList.textContent = todoInput.value;
+        createNewTask();
+        ulList.append(liList);
+        todoInput.value = ''
+        errorInfo.textContent = 'podaj kolejne zadanie'
+    }else{
+        errorInfo.textContent = 'Podaj nowe zadanie'
+        
+    }
+}
+
+const enterEvent = () =>{
+    if(event.key === 'Enter' || event.keyCode === 13){
+addTaskBtn()
+    }
+}
+const createNewTask = () =>{
+    const btnPanel = document.createElement('div');
+    btnPanel.classList.add('tools');
+    liList.append(btnPanel);
+
+    const confirmBtn = document.createElement('button');
+    confirmBtn.classList.add('complete');
+    confirmBtn.innerHTML = '<i class="fas fa-check"></i>'
+
+    const editBtn = document.createElement('button');
+    editBtn.classList.add('edit');
+    editBtn.textContent = 'EDYTUJ'
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('delete');
+    deleteBtn.innerHTML = '<i class="fas fa-times"></i>'
+
+    btnPanel.append(confirmBtn, editBtn, deleteBtn)
 
 }
 
@@ -20,7 +61,4 @@ const DOMEvents = ()=>{
 
 
 
-
-
-
-document.activeElement('DOMcontenctLoaded', main);
+document.addEventListener('DOMContentLoaded', main);
