4a11f6292020d44c4c1c9c6bad5e56216a8a2490
Andrzej Gebala <andrzej.gebala35@gmail.com>
2021-10-08 11:15:12 +0200

    pobranie danym z html do js

---
+++ js.js
@@ -0,0 +1,26 @@
+let todoInput, errorInfo, addBtn, ulList; 
+
+const main = ()=>{
+    DOMElement();
+    DOMEvents();
+}
+
+const DOMElement = ()=>{
+    todoInput = document.querySelector('.todo-input');
+    errorInfo = document.querySelector('.error-info');
+    addBtn = document.querySelector('.btn-add');
+    ulList = document.querySelector('.todolist ul');
+}
+
+const DOMEvents = ()=>{
+
+}
+
+
+
+
+
+
+
+
+document.activeElement('DOMcontenctLoaded', main);
